d1c80e6032500edff22b49a358978c40284a20d2
Matt <mra286@humboldt.edu>
2020-02-07 16:37:09 -0700

    Stub JPA and REST projects, with project dependencies

---
+++ DroneTracker/src/main/java/com/skilldistillery/eventtracker/DroneTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.eventtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroneTrackerApplication.class, args);
+	}
+
+}
